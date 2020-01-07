e5abb09a2890b4a0d2413336241564323d5eb46c
Eliza Ahmed <elizarafee@gmail.com>
2020-01-07 10:28:23 +0000

    updated registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,10 +26,13 @@
 
 
                         <div class="form-group row">
-                            <label for="first_name" class="col-md-4 col-form-label text-md-right">First Name <span class="text-danger">*</span></label>
+                            <label for="first_name" class="col-md-4 col-form-label text-md-right">First Name <span
+                                    class="text-danger">*</span></label>
 
                             <div class="col-md-6">
-                                <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" autofocus>
+                                <input id="first_name" type="text"
+                                    class="form-control @error('first_name') is-invalid @enderror" name="first_name"
+                                    value="{{ old('first_name') }}" autofocus>
 
                                 @error('first_name')
                                 <span class="invalid-feedback" role="alert">
@@ -40,10 +43,13 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="last_name" class="col-md-4 col-form-label text-md-right">Last Name <span class="text-danger">*</span></label>
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">Last Name <span
+                                    class="text-danger">*</span></label>
 
                             <div class="col-md-6">
-                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}">
+                                <input id="last_name" type="text"
+                                    class="form-control @error('last_name') is-invalid @enderror" name="last_name"
+                                    value="{{ old('last_name') }}">
 
                                 @error('last_name')
                                 <span class="invalid-feedback" role="alert">
@@ -54,11 +60,15 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address <span class="text-danger">*</span></label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address <span
+                                    class="text-danger">*</span></label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
-                                <small class="form-text text-info">We will send account verification email and all notification to this email, and you will not be able to update this email without verification.</small>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" autocomplete="email">
+                                <small class="form-text text-info">We will send account verification email and all
+                                    notification to this email, and you will not be able to update this email without
+                                    verification.</small>
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -70,10 +80,13 @@
 
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password <span class="text-danger">*</span></label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Password <span
+                                    class="text-danger">*</span></label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    autocomplete="new-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -84,33 +97,35 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password <span class="text-danger">*</span></label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password
+                                <span class="text-danger">*</span></label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control"
+                                    name="password_confirmation" autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4 border border-info rounded bg-light p-3">
 
-                                <div class="form-check @error('type') is-invalid @enderror mb-1">
-                                    <input class="form-check-input" type="radio" name="type" id="type1" value="2" @if(old('type') == 2) checked @endif>
-                                    <label class="form-check-label" for="type1">
-                                        I am registering as a Tutor
-                                    </label>
-                                </div>
-                                <div class="form-check @error('type') is-invalid @enderror">
-                                    <input class="form-check-input" type="radio" name="type" id="type2" value="3" @if(old('type') == 3) checked @endif>
-                                    <label class="form-check-label" for="type2">
-                                    I am registering to find a Tutor
-                                    </label>
-                                </div>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Registering as
+                                <span class="text-danger">*</span></label>
 
+                            <div class="col-md-6">
+                                <div class="custom-control @error('type') is-invalid @enderror custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadioInline1" name="customRadioInline1"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadioInline1">Tutor</label>
+                                </div>
+                                <div class="custom-control @error('type') is-invalid @enderror custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadioInline2" name="customRadioInline1"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadioInline2">Guardian</label>
+                                </div>
 
                                 @error('type')
                                 <span class="invalid-feedback" role="alert">
-                                    <strong>Please let us know why you are registering</strong>
+                                    <strong>Please let us know who you are.</strong>
                                 </span>
                                 @enderror
                             </div>
@@ -128,5 +143,3 @@
     </div>
 </div>
 @endsection
-
-
