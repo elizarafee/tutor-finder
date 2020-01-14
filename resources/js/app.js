93f2067ee4d1744d9f336d672524b040498dec1e
Eliza Ahmed <elizarafee@gmail.com>
2020-01-14 20:59:35 +0000

    code cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,30 +10,31 @@ import * as suggestags from 'suggestags';
 import axois from 'axios';
 
 
-
-
+// Requesting route to get subjects  from AjaxController 
 axios.get('/ajax/get-subjects')
   .then(function (response) {
 
+    // successful - adding returned data to suggesttag 
     $('.subject-tags').amsifySuggestags({
         type : 'amsify',
-        suggestions: response.data,
+        suggestions: response.data, // subjects array 
         whiteList: true
     });
-
-    //console.log();
 
   })
   .catch(function (error) {
     console.log(error);
   });
 
+
+  // Requesting route to get locations  from AjaxController 
   axios.get('/ajax/get-locations')
   .then(function (response) {
 
+    // successful - adding returned data to suggesttag 
     $('.location-tags').amsifySuggestags({
         type : 'amsify',
-        suggestions: response.data,
+        suggestions: response.data, // locations array 
         whiteList: true
     });
 
@@ -42,17 +43,16 @@ axios.get('/ajax/get-subjects')
     console.log(error);
   });
 
+  // adding classes as tag 
   $('.class-tags').amsifySuggestags({
     type : 'amsify',
     suggestions: ['Play Group','Class 1','Class 2','Class 3','Class 4','Class 5','Class 6','Class 7','Class 8','Class 9','Class 10','HSC Year 1','HSC Year 2'],
     whiteList: true
 });
 
+// self invoking function of jquery
 $(function () {
-
     $('.pagination').addClass('justify-content-center');
-  
-      
 });
 
 
