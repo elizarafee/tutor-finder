78500b05c724c4b3aa84fb48a84b2613b9c4d69d
Eliza Ahmed <elizarafee@gmail.com>
2020-02-10 19:47:13 +0000

    html enhanced

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8 text-center">
            <h4>An intuitive application to connect potential tutors with student's guardians.</h4>
-           <img class="w-50" src="{{ url('images/connections.png') }}"/>
+           <img class="w-50" alt="Tutor Finder" src="{{ url('images/connections.png') }}"/>
            <p class="mt-3">Please register and complete your profile to find and make connection with each other.</p>
         </div>
     </div>
